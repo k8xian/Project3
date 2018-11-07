eaa27eb01ad067bb2e79acb98542631f0511ad23
k8xian <kate@innovationsforlearning.org>
2018-11-06 20:50:33 -0600

    adding refactored login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
+
 const App = () => (
   <Router>
     <div>
-      <Nav />
       <Switch>
-        <Route exact path="/" component={Books} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/old-home" component={Books} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/books/:id" component={Detail} />
+        <Route exact path="/profile/:id" component={Profile} />
+        <Route exact path="/profile/:id/edit" component={Profile} />
         <Route component={NoMatch} />
       </Switch>
     </div>
