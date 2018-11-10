21706cc72efd4e2b0bad55511a43c4dd8fa22f84
Ciwonie <ciwonie@gmail.com>
2018-11-10 12:41:01 -0600

    added user export in models/index.js

--- models/index.js
+++ models/index.js
@@ -2,5 +2,6 @@ module.exports = {
     //put your models here, aka:
     //Schemaname: require("./schemaname")
     // GoogleUser: require("./google-model.js"),
+    user: require("./user"),
     userGameIDs: require("./usersGameIDsModel"),
 };
