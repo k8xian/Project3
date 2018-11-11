13d231454529808f80320d5be7e3f07079eaee40
k8xian <kate@innovationsforlearning.org>
2018-11-11 12:34:16 -0600

    making game buttons functional and adding titles and refresh buttons to stat bar.

--- client/src/components/Stats/AllStats.js
+++ client/src/components/Stats/AllStats.js
@@ -4,27 +4,52 @@ import { Stat } from '../Elements/index'
 
 const StyledStatBlock = styled.ul`
 columns: 4;
-width: 90%;
-margin: 20px auto;
+width: 100%;
+margin: 20px 0;
 list-style-type: none;
+`
+const StatWrapper = styled.div`
+width: 90%;
+margin: auto;
+`
+const StatTitle = styled.h2`
+font-weight: 300;
+`
+
+const StyledRefreshStats = styled.button`
+clear: both;
+display: block;
+width: 100px;
+height: 20px;
+background-color: rgba(0,0,0,0);
+border-radius: 0;
+border: 1px solid #00fff4;
+color: #00fff4;
+cursor: pointer;
+margin: 20px 0;
+float: left;
 `
 
 // add statname= and statvalue= to each prop stat to define a stat
 // use component will mount to map out the data here
 const Allstats = props => (
-    <StyledStatBlock>
-        <Stat statname="allstats" statvalue="10000" />
-        <Stat />
-        <Stat />
-        <Stat />
-        <Stat />
-        <Stat />
-        <Stat />
-        <Stat />
-        <Stat />
-        <Stat />
-        <Stat />
-    </StyledStatBlock>
+    <StatWrapper>
+        <StatTitle>All Stats</StatTitle>
+        <StyledStatBlock>
+            <Stat statname="allstats" statvalue="10000" />
+            <Stat />
+            <Stat />
+            <Stat />
+            <Stat />
+            <Stat />
+            <Stat />
+            <Stat />
+            <Stat />
+            <Stat />
+            <Stat />
+        </StyledStatBlock>
+        <StyledRefreshStats>Refresh Stats</StyledRefreshStats>
+    </StatWrapper>
 );
 
 export default Allstats;
