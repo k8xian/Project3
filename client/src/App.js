567c55ee1cc93e75967ca87fbf225c746cf99d2b
k8xian <11559298+k8xian@users.noreply.github.com>
2018-11-16 12:03:51 -0600

    cleaning up App.js, removing the rest of redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
+//react components
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {Spinner} from "@blueprintjs/core";
+
+//local components
 import Profile from "./pages/Profile";
 import PublicProfile from "./pages/Profile/PublicProfile";
 import Login from "./pages/Login";
 import Logout from "./components/Logout";
 import {app, base} from "./Base";
-// import SignUp from "./components/SignUp";
-// import SignIn from "./components/SignIn";
-
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import reduxThunk from 'redux-thunk';
-import axios from 'axios';
-
-import Home from "./components/Home";
-import Dashboard from "./components/Dashboard";
-import reducers from './reducers';
-
-import authGuard from './components/HOCs/authGuard'
 
 
 class App extends Component {
@@ -90,7 +79,6 @@ class App extends Component {
             <Route exact path="/profile/:id" component={Profile} />
             {/* This will be the individual view for any profile with an edit button that will show/hide forms */}
             <Route exact path="/profile/:id/edit" component={Profile} />
-            <Route exact path="/dashboard" component={authGuard(Dashboard)} />
           </Switch>
         </div>
       </Router>
